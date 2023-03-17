4abc33bcd27e84c5a0bb2853d2348b722019c243
Brody <brodypen7987@gmail.com>
2023-03-17 12:59:39 -0500

    Opposite numbers

---
+++ 8kyu/opposite-number.ts
@@ -0,0 +1,19 @@
+/*
+What I've learned:
+Answers can be much simpler than you think.
+
+*/
+
+// export class Kata {
+//   static opposite(n: number) {
+//     if (n > 0) {
+//       return -n;
+//     }
+//     return Math.abs(n);
+//   }
+// }
+export class Kata {
+  static opposite(n: number) {
+    return -n;
+  }
+}
